d1692fdfb5a2f901818d64353ec20b23d378f937
David Raison <david@tentwentyfour.lu>
2018-07-29 21:57:57 +0200

    Adapt package prefix to php version

--- manifests/globals.pp
+++ manifests/globals.pp
@@ -76,14 +76,14 @@ class php::globals (
         }
       } else {
         case $globals_php_version {
-          /^7/: {
+          /^7\.[0-9]/: {
             $default_config_root  = "/etc/php/${globals_php_version}"
             $default_fpm_pid_file = "/var/run/php/php${globals_php_version}-fpm.pid"
             $fpm_error_log        = "/var/log/php${globals_php_version}-fpm.log"
             $fpm_service_name     = "php${globals_php_version}-fpm"
             $ext_tool_enable      = "/usr/sbin/phpenmod -v ${globals_php_version}"
             $ext_tool_query       = "/usr/sbin/phpquery -v ${globals_php_version}"
-            $package_prefix       = 'php7.0-'
+            $package_prefix       = "php${globals_php_version}-"
           }
           default: {
             $default_config_root  = '/etc/php5'
